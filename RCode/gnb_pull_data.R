fe9f8639e72f2e57d86b26b26bf9202d12f84551
dasalm20 <dasalm20@gmail.com>
2021-04-27 17:39:36 -0400

    These are changes that I made to the code based on my google account credentials.

--- RCode/gnb_pull_data.R
+++ RCode/gnb_pull_data.R
@@ -23,46 +23,141 @@ library(osmdata)
 library(dplyr)
 
 
-gnb.base <- sf::st_read(dsn = "GNB_2021", layer = "afr_mli_l04_2002")
+gnb.base <- sf::st_read(dsn = "GNB_2021", layer = "afr_gnb_l04_2002")
 
 gnb.grid <- SAEplus::gengrid(dsn = "GNB_2021", layer = "afr_gnb_l04_2002",
-                             raster_tif = "gnb_ppp_2020_UNadj_constrained.tif")
+                            raster_tif = "gnb_ppp_2020_UNadj_constrained.tif",
+                            grid_shp = T,
+                            featname = "bld_count",
+                           drop_Zero = F)
 
 
 sf::st_write(gnb.grid$polygon_dt, dsn = "GNB_2021", layer = "gnb_poppoly", driver = "ESRI Shapefile")
 
-gnb.ntl <- SAEplus::gee_datapull(gee_boundary = "users/ifeanyiedochie/afr_gnb_l04_2002",
-                                 gee_polygons = "users/ifeanyiedochie/gnb_poppoly",
+gnb.ntl <- SAEplus::gee_datapull(email = "dasalm20@gmail.com",
+                                 gee_boundary = "users/dasalm20/afr_gnb_l04_2002",
+                                 gee_polygons = "users/dasalm20/gnb_poppoly",
                                  gee_datestart = "2018-09-01",
                                  gee_dateend = "2018-12-31",
-                                 gee_desc = "TCD_NTL_2018SepDec",
-                                 ldrive_dsn = "TCD_2021/TCD_NTL_2018SepDec")
+                                 gee_desc = "GNB_NTL_2018SepDec",
+                                 ldrive_dsn = "GNB_2021/GNB_NTL_2018SepDec")
 
-gnb.ntl2 <- SAEplus::gee_datapull(gee_boundary = "users/ifeanyiedochie/afr_gnb_l04_2002",
-                                  gee_polygons = "users/ifeanyiedochie/gnb_poppoly",
+gnb.ntl2 <- SAEplus::gee_datapull(email = "dasalm20@gmail.com",
+                                  gee_boundary = "users/dasalm20/afr_gnb_l04_2002",
+                                  gee_polygons = "users/dasalm20/gnb_poppoly",
                                   gee_datestart = "2019-04-01",
-                                  gee_dateend = "2019-06-31",
-                                  gee_desc = "TCD_NTL_2019AprJun",
-                                  ldrive_dsn = "TCD_2021/TCD_NTL_2019AprJun")
+                                  gee_dateend = "2019-06-30",
+                                  gee_desc = "GNB_NTL_2019AprJun",
+                                  ldrive_dsn = "GNB_2021/GNB_NTL_2019AprJun")
+
+##name the ntl indicator real quick
+ gibntl_sepdec.dt <- sf::st_read(dsn = "GNB_2021", layer = "GNB_NTL_2018SepDec_2021_04_08_12_56_31")
+gnbntl_aprjun.dt <- sf::st_read(dsn = "GNB_2021", layer = "GNB_NTL_2019AprJun_2021_04_08_12_50_58")
+#
+names(gnbntl_aprjun.dt)[names(gnbntl_aprjun.dt) == "mean"] <- "mean_ntlaprjun19"
+ames(gnbntl_sepdec.dt)[names(gnbntl_sepdec.dt) == "mean"] <- "mean_ntlsepdec18"
+#
+#
+
+gnbno2_sepdec.dt <- sf::st_read(dsn = "GNB_2021", layer = "GNB_NO2_2018SepDec_2021_04_08_13_38_59")
+ names(gnbno2_julsep.dt)[names(ginno2_julsep.dt) == "mean"] <- "mean_no2sepdec18"
+#
+gnb.no21 <- SAEplus::gee_datapull( email = "dasalm20@gmail.com",
+                                 gee_boundary = "users/dasalm20/afr_gnb_l04_2002",
+                                   gee_polygons = "users/dasalm20/gnb_poppoly",
+                                 gee_band = "tropospheric_NO2_column_number_density",
+                                  gee_dataname = "COPERNICUS/S5P/NRTI/L3_NO2",
+                                  gee_datestart = "2019-04-01",
+                                   gee_dateend = "2019-06-30",
+                                  gee_desc = "GNB_NO2_2019AprJun",
+                                 ldrive_dsn = "GNB_2021/GIN_NO2_2019AprJun")
+# ##pull in the landcover data
+SAEplus::gee_datapull(email = "dasalm20@gmail.com",
+                      gee_boundary = "users/dasalm20/afr_gnb_l04_2002",
+                      gee_polygons = "users/dasalm20/gnb_poppoly",
+                      gee_band = c("tree-coverfraction","urban-coverfraction","grass-coverfraction",
+                                   "shrub-coverfraction","crops-coverfraction","bare-coverfraction",
+                                   "water-permanent-coverfraction","water-seasonal-coverfraction",
+                                   "moss-coverfraction"),
+                     gee_dataname = "COPERNICUS/Landcover/100m/Proba-V-C3/Global",
+                      gee_datestart = "2018-09-01",
+                      gee_dateend = "2018-12-31",
+                      gee_desc = "GNB_LC_2018SepDec",
+                      ldrive_dsn = "GNB_2021/GIN_LC_2018JulSep")
+
+SAEplus::gee_datapull(email = "dasalm20@gmail.com",
+                        gee_boundary = "users/dasalm20/afr_gnb_l04_2002",
+                       gee_polygons = "users/dasalm20/gnb_poppoly",
+                      gee_band = c("tree-coverfraction","urban-coverfraction","grass-coverfraction",
+                                   "shrub-coverfraction","crops-coverfraction","bare-coverfraction",
+                                   "water-permanent-coverfraction","water-seasonal-coverfraction",
+                                   "moss-coverfraction"),
+                      gee_dataname = "COPERNICUS/Landcover/100m/Proba-V-C3/Global",
+                      gee_datestart = "2019-04-01",
+                      gee_dateend = "2019-06-30",
+                      gee_desc = "GNB_LC_2019AprJun",
+                      ldrive_dsn = "GNB_2021/GNB_LC_2019AprJun")
+#
+#
+# ## pull the data on impervious surface
+ SAEplus::gee_pullimage(email = "dasalm20@gmail.com",
+                       gee_polygons = "users/dasalm20/gnb_poppoly",
+                       gee_band = "change_year_index",
+                       gee_dataname = "Tsinghua/FROM-GLC/GAIA/v10",
+                       gee_desc = "GNB_IS_2018SepDec",
+                       ldrive_dsn = "GNB_2021/GNB_IS_2018SepDec")
+#
+## include other GEE data on CO, global human modification, gridmet drought data
+SAEplus::gee_datapull( email = "dasalm20@gmail.com",
+                     gee_boundary = "users/dasalm20/afr_gnb_l04_2002",
+                     gee_polygons = "users/dasalm20/gnb_poppoly",
+                      gee_band = c("CO_column_number_density", "H2O_column_number_density",
+                                   "cloud_height"),
+                      gee_dataname = "COPERNICUS/S5P/NRTI/L3_CO",
+                      gee_datestart = "2018-09-01",
+                      gee_dateend = "2018-12-31",
+                      gee_desc = "GNB_CO_2018JulSep",
+                      ldrive_dsn = "GNB_2021/GNB_CO_2018SepDec",
+                      gee_crs = "WGS84")
+#
+SAEplus::gee_datapull(email = "dasalm20@gmail.com"
+                      gee_boundary = "users/dasalm20/afr_gnb_l04_2002",
+                       gee_polygons = "users/dasalm20/gnb_poppoly",
+                      gee_band = c("tree-coverfraction","urban-coverfraction","grass-coverfraction",
+                                   "shrub-coverfraction","crops-coverfraction","bare-coverfraction",
+                                   "water-permanent-coverfraction","water-seasonal-coverfraction",
+                                   "moss-coverfraction"),
+                       gee_dataname = "COPERNICUS/Landcover/100m/Proba-V-C3/Global",
+                      gee_datestart = "2019-04-01",
+                      gee_dateend = "2019-06-30",
+                      gee_desc = "GNB_LC_2019AprJun",
+                     ldrive_dsn = "GNB_2021/GNB_LC_2019AprJun")
+
+
+
+
+
+
+
 
 available.dt <- SAEplus::wpopbuilding_vcheck()
 gnb.building <- SAEplus::wpopbuilding_pull(iso = "GNB", ldrive_dsn = "GNB_2021", wpversion = "v1.1")
 
 gnb.osm <- SAEplus::osm_datapull(country = "Guinea Bissau",
-                                 ldrive = "C:/Users/ifean/Documents/WorldBankWork/SAEPlus_Other")
+                                 ldrive = "/Users/daylansalmeron/Documents/R_git_pro/SAEPlus_other")
 
 gnb.lines <- SAEplus::osm_processlines(shapefile_path = "GNB_2021/gnb_poppoly.shp",
-                                       osm_path = "C:/Users/ifean/Documents/WorldBankWork/SAEPlus_Other/Guinea Bissau_osmlines")
+                                       osm_path = "/Users/daylansalmeron/Documents/R_git_pro/SAEPlus_other/Guinea_Bissau_osmlines")
 saveRDS(gnb.lines, file = "./../S2S-REMDI/GNB_2021/GNB_lines_obj")
 
 
 gnb.mp <- SAEplus::osm_processmp(shapefile_path = "GNB_2021/gnb_poppoly.shp",
-                                 osm_path = "C:/Users/ifean/Documents/WorldBankWork/SAEPlus_Other/Guinea Bissau_osmmp")
+                                 osm_path = "/Users/daylansalmeron/Documents/R_git_pro/SAEPlus_other/Guinea_Bissau_osmmp")
 
 saveRDS(gnb.mp, file = "./../S2S-REMDI/GNB_2021/GNB_mp_obj")
 
 gnb.points <- SAEplus::osm_processpoints(shapefile_path = "GNB_2021/gnb_poppoly.shp",
-                                         osm_path = "C:/Users/ifean/Documents/WorldBankWork/SAEPlus_Other/Guinea Bissau_osmpoints")
+                                         osm_path = "/Users/daylansalmeron/Documents/R_git_pro/SAEPlus_other/Guinea_Bissau_osmpoints")
 
 saveRDS(gnb.points, file = "./../S2S-REMDI/GNB_2021/GNB_points_obj") #save the object as RData
 
